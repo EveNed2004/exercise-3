f5ff6e7dd72ae3809855a2e5e90897f0daa6f20f
EveieNedeljak <eveie.nedeljak@ueth.ca>
2024-11-26 14:46:16 -0700

    fix image

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,30 @@ console.log('hello');
 
 $("#button-1").on("click", function(){
     $(this).addClass("button-active")
-    $(".zalea").html("<img src='../images/base.PNG'>")
+    $(".zalea").html("<img src='../images/base.png'>")
     $("#button-2, #button-3, #button-4, #button-5").removeClass("button-active");
 });
 
 $("#button-2").on("click", function(){
     $(this).addClass("button-active")
-    $(".zalea").html("<img src='../images/1st-gen.PNG'>")
+    $(".zalea").html("<img src='../images/1stgen.png'>")
     $("#button-1, #button-3, #button-4, #button-5").removeClass("button-active");
 });
 
 $("#button-3").on("click", function(){
     $(this).addClass("button-active")
-    $(".zalea").html("<img src='../images/2nd-gen.PNG'>")
+    $(".zalea").html("<img src='../images/2ndgen.png'>")
     $("#button-2, #button-1, #button-4, #button-5").removeClass("button-active");
 });
 
 $("#button-4").on("click", function(){
     $(this).addClass("button-active")
-    $(".zalea").html("<img src='../images/3rd-gen.PNG'>") 
+    $(".zalea").html("<img src='../images/3rdgen.png'>") 
     $("#button-2, #button-3, #button-1, #button-5").removeClass("button-active");
 });
 
 $("#button-5").on("click", function(){
     $(this).addClass("button-active")
-    $(".zalea").html("<img src='../images/4th-gen.PNG'>")
+    $(".zalea").html("<img src='../images/4thgen.png'>")
     $("#button-2, #button-3, #button-1, #button-4").removeClass("button-active");
 });
